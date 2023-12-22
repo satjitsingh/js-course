4b2acc98da7e181ec48a54b27ca29a386eccca8a
satjitsingh <66747012+satjitsingh@users.noreply.github.com>
2023-12-22 09:57:02 +0000

    datatypes and conversion

---
+++ .devcontainer/01-basics/datatypes.js
@@ -0,0 +1,45 @@
+let name = "Satjit Singh"
+let age = 12
+let isTrue = false
+let state;
+
+//undefined is a type, whereas null is an object because it's type is object.
+
+//----------------------
+
+
+// let score = "false"
+
+
+// console.log(typeof(score))
+// console.log(typeof score)
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // returns NaN 'not a number'
+
+// "33" => 33
+// "33abc" => NaN, which has type number
+// true => 1; false => 0
+
+// let isLoggedIn = "satjit"
+
+// let booleanIsLoogedIn = Boolean(isLoggedIn);
+
+// console.log(typeof booleanIsLoogedIn)
+// console.log(booleanIsLoogedIn)
+
+// 1=> true; 0 => false
+// "" => false
+// "satjit" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
